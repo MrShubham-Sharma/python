7f607c42443ba628b3aefa1caf2abeb359d37c45
MrShubham-Sharma <s8974477@gmail.com>
2025-09-03 23:46:03 +0530

    new programs

---
+++ Oprator.py
@@ -0,0 +1,28 @@
+# a assigned value 5
+a=5 
+# b assigned value of 6
+b=6 
+# prints value addition of a+b
+print(a+b)
+# increment oprators with value b incremeted by 3
+b+=3 
+a-=2 #a was decremented by 2
+print( b)
+print(a)
+print(a-b)
+
+# logical oprator
+#or oprator
+e= True or False
+print("True or False is",True or False)
+print("False or True is",False or True)
+print("True or True is ",True or True)
+print("False or False is ",False or False)
+# and oprator 
+print("True and False is",True and False)
+print("False and True is",False and True)
+print("True and True is ",True and True)
+print("False and False is ",False and False)
+# not oprator change the conditions where true to false or false to true 
+print(not(False))
+print(not(True))
