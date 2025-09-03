9353f3ec8297719d6f56e2537631302e82ee9819
MrShubham-Sharma <s8974477@gmail.com>
2025-09-04 00:00:03 +0530

    new programs

---
+++ Variable.py
@@ -0,0 +1,15 @@
+# name is string 
+name = "MR.SHUBHAM"
+# age is floting value
+age = 20.2
+# proffesion is string 
+proffesion = "engineering"
+# Sallary is integer 
+Sallary = 200000 
+
+# now it prints the objectives
+print( name ,age, proffesion, Sallary)
+# then prints the sallery * age 
+print( age*Sallary)
+#variable must be start with alphabet and underscore
+#variable can't start with digit
