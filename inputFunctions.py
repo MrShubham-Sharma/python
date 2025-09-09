6d6fe6ad13cdf3683fd32d89c727110fd974cdb7
MrShubham-Sharma <s8974477@gmail.com>
2025-09-09 19:46:43 +0530

    new function

---
+++ inputFunctions.py
@@ -0,0 +1,14 @@
+#The Code for Inpute opration
+# with input we can set the input
+a = input("Enter The Number 1: ") #it will take the number 1 input
+b = input("Enter The Number 2: ") #it will take the number 2 input
+print("Number is : ",a)
+print("Number is : ",b)
+print("sum is : ",a+b) #the addition will added into string 
+
+#Code for integer value with input
+c = int(input("Enter The Number 1: "))
+d = int(input("Enter The Number 2: "))
+print("Number c is : ",c)
+print("Number d is : ",d)
+print("Sum is ", c+d)
