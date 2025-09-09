427ae9d28512517bede0f5fbe797d8eb5c5f8705
MrShubham-Sharma <s8974477@gmail.com>
2025-09-09 19:21:40 +0530

    new oprations

--- Oprator.py
+++ Oprator.py
@@ -25,4 +25,48 @@ print("True and True is ",True and True)
 print("False and False is ",False and False)
 # not oprator change the conditions where true to false or false to true 
 print(not(False))
+print(not(True))# a assigned value 5
+a=5 
+# b assigned value of 6
+b=6 
+# prints value addition of a+b
+print(a+b)
+# increment oprators with value b incremeted by 3
+b+=3 
+a-=2 #a was decremented by 2
+print( b)
+print(a)
+print(a-b)
+
+# logical oprator
+#or oprator
+e= True or False
+print("True or False is",True or False)
+print("False or True is",False or True)
+print("True or True is ",True or True)
+print("False or False is ",False or False)
+# and oprator 
+print("True and False is",True and False)
+print("False and True is",False and True)
+print("True and True is ",True and True)
+print("False and False is ",False and False)
+# not oprator change the conditions where true to false or false to true 
+print(not(False))
 print(not(True))
+# type of oprators
+g= 232
+t= type(g)
+print(t)
+# for string oprators 
+h = "131" #it will show type 
+h = type(h)
+print(h)
+# for float
+p=31.4
+p =type(p)
+print(p)
+# if number in the string we can change the its type 
+l="29.4"
+r = float(l)
+c= type(r)
+print(c)
